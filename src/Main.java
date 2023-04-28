5aa0932ae560aa898c418119220a58b94938f58f
Barbara <bahrodrigues620@gmail.com>
2023-04-28 15:54:10 -0300

    Mentoria + curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHorario(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Js");
+        curso2.setDescricao("Descrição Curso Js");
+        curso2.setCargaHorario(8);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
+
 }
